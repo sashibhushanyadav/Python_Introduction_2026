20b8ab75ba61a41d8da9c3011bc95c9f644ec6e5
sashibhushanyadav <contact.sashibhushan@gmail.com>
2026-01-15 08:09:05 +0545

    fea: theory portion

---
+++ D1/index.py
@@ -0,0 +1,73 @@
+print('Hello Sashi')
+
+# Character Set
+# 1. ASCII (American Standard Code for Information Interchange)
+# 2. Unicode (Universal Character set)
+
+# Variable
+# 1. it cannot start with digit such as 1sashi, 5hello
+# 2. we can't use special symbols like !,@,#,$ etc as variables name
+
+# Data Types
+# 1. Number (Integer, Float)
+# 2. String
+# 3. Boolean
+# 4. None
+# 5. Sequence (List, Tuple, Range)
+# 6. Mapping (Dictionary)
+# 7. Set (set)
+# -- Go through with Method, and addition function and mechanism of those data types
+
+# Reserved Keywords
+
+# and, as, assert, break, continue, class, def, del, if, elif, else, except, finally,
+# True, False, for, from, global, import, in is, lambda, nonlocal, None, not, or, pass,
+# raise, return, try, with, while, yield
+
+# Types of Operators
+# 1. Arithmetic Operator
+#     -Addition
+#     -Subtraction
+#     -Multiplication
+#     -Division
+#     -Modulus
+#     -Trucate Division
+#     -Double Multiplication
+    
+# 2. Comparison Operators
+#     ==
+#     !=
+#     <
+#     >
+#     >=
+#     <=
+    
+# 3. Assignment Operators
+#     =
+#     +=
+#     -=
+#     %= and go on....
+    
+# 4. Logical Operators
+#     not
+#     and
+#     or
+
+# Type Conversion
+# 1. Implicit Type conversion / Automatic
+
+x = 2           # integer
+y = 3.5         # float
+
+print(x+y)
+output = 5.5    # float (it gives float data type by itself)
+
+# 2. Explicit Type conversion (Type casting) / Mannual
+
+a = '12'    # string
+b = 3       # int
+c = int(a)  # convert string into integer
+
+print(b+c)
+output = 15 # integer (converting the value of sting into int
+            # which is in 'a' variable then added which gives same datatype output)
