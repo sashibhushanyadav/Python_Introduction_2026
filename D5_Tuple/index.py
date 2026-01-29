b8665e48bf01f3719b87f8cd27c72215502ea6b9
sashibhushanyadav <contact.sashibhushan@gmail.com>
2026-01-29 06:39:54 +0545

    feat: learn about tuple and their methods

---
+++ D5_Tuple/index.py
@@ -0,0 +1,13 @@
+a = (1, 2, 6, 4, 10, 2, 2)
+
+count = a.count(2) # Count the elements
+print(count)
+
+ind = a.index(6) # findout the index
+print(ind)
+
+# length = a.len()
+print(len(a))
+print(max(a))
+print(min(a))
+print(sum(a))
